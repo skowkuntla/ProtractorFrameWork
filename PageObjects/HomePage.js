3d371f2c453774fd45a3336267b7f5ce62495432
skowkuntla <sainathgoud.kowkuntla@factset.com>
2021-04-20 18:47:41 +0530

    First Protractor commit

---
+++ PageObjects/HomePage.js
@@ -0,0 +1,32 @@
+const { browser } = require("protractor");
+
+let HomePage = function(){
+
+      let firstnumber_input = element(by.model('first'));
+      let secondnumber_input = element(by.model('second'));
+      let goButton = element(by.css('[ng-click="doAddition()"]'));
+
+      this.get = function(url){
+            browser.get(url);
+      };
+
+      this.enterFirstNumber = function(firstNo){
+            firstnumber_input.sendKeys(firstNo);
+      };
+
+      this.enterSecondNumber = function(secondNo){
+            secondnumber_input.sendKeys(secondNo);
+      };
+
+      this.clickGo = function(){
+            goButton.click();
+      };
+
+      this.verifyResult = function(result){
+            let output = element(by.cssContainingText('.ng-binding', result));
+            expect(output.getText()).toEqual(result);
+      };
+
+};
+
+module.exports = new HomePage();
---
+++ tests/calculator_spec.js
@@ -0,0 +1,39 @@
+const {browser, element} = require("protractor");
+
+let HomePage = require('../PageObjects/HomePage')
+
+describe('demo calculator tests', function () {
+
+      it('addition tests', function () {
+
+            HomePage.get('https://juliemr.github.io/protractor-demo/');
+
+            HomePage.enterFirstNumber('4');
+
+            HomePage.enterSecondNumber('5');
+
+            HomePage.clickGo();
+
+            HomePage.verifyResult('9');
+
+            browser.sleep(2000)
+
+      });
+
+       it('subtraction tests', function () {
+
+             HomePage.get('https://juliemr.github.io/protractor-demo/');
+
+             HomePage.enterFirstNumber('4');
+
+             HomePage.enterSecondNumber('5');
+
+             HomePage.clickGo1();
+
+             HomePage.verifyResult('9');
+
+             browser.sleep(2000)
+
+       });
+
+});
